673c0062087f79d2eaf3c61589640cc437138c2d
unknown <nguyenhuyenla209@gmail.com>
2025-12-27 15:48:02 +0700

    fix product service3

--- src/main/java/com/nguyenthithuhuyen/example10/config/CloudinaryConfig.java
+++ src/main/java/com/nguyenthithuhuyen/example10/config/CloudinaryConfig.java
@@ -3,13 +3,28 @@ package com.nguyenthithuhuyen.example10.config;
 import com.cloudinary.Cloudinary;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class CloudinaryConfig {
 
+    @Value("${cloudinary.cloud_name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api_key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api_secret}")
+    private String apiSecret;
+
     @Bean
     public Cloudinary cloudinary() {
-        // Tự động đọc CLOUDINARY_URL từ environment
-        return new Cloudinary(System.getenv("CLOUDINARY_URL"));
+        Map<String, String> config = new HashMap<>();
+        config.put("cloud_name", cloudName);
+        config.put("api_key", apiKey);
+        config.put("api_secret", apiSecret);
+        return new Cloudinary(config);
     }
 }
