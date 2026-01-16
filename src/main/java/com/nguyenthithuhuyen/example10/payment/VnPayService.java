8849756f42de28cb34c072e5add63e5aea945d23
cuong0374522557-beep <cuong.0374522557@gmail.com>
2026-01-16 08:48:54 +0700

    Up code

--- src/main/java/com/nguyenthithuhuyen/example10/payment/VnPayService.java
+++ src/main/java/com/nguyenthithuhuyen/example10/payment/VnPayService.java
@@ -45,7 +45,8 @@ public class VnPayService {
         params.put("vnp_OrderInfo", "Thanh toan don hang " + order.getId());
         params.put("vnp_OrderType", "other");
         params.put("vnp_Locale", "vn");
-        params.put("vnp_IpAddr", "0.0.0.0");
+        params.put("vnp_IpAddr", "127.0.0.1");
+;
         params.put("vnp_ReturnUrl", returnUrl);
 
         params.put("vnp_CreateDate",
